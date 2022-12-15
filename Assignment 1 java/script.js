44ac027300e7af2808c044e6d5bde855f59133dc
Array.js <abhinandanchougule3777@gmail.com>
2022-12-15 16:41:56 +0530

    javascript-tasks

---
+++ Assignment 1 java/script.js
@@ -0,0 +1,93 @@
+// alert("Hello");
+
+
+//Q1
+// var myName ="Abhinandan";
+// console.log("myname");
+// console.log("myname");
+// console.log("myname");
+
+
+
+//Q2
+// let boolean = true;
+// console.log("boolean");
+// let string = "string";
+// console.log(string);
+// let a =15;
+// console.log(a);
+
+//Q3
+// let firstname = "prathamesh";
+// console.log(firstname);
+
+// //Q4
+// let z = 15+15;
+// alert(z);
+
+// //Q5
+// let Y2 = 80;
+// console.log(Y2);
+
+// //Q6
+// let z1 = 15*2;
+// console.log(z1);
+
+// //Q7
+// let g2 = 40/2;
+// console.log(g2);
+
+// //Q8
+// let k4 = 19+0;
+// console.log(k4);
+
+// //Q9 ++ increment
+// let c12 = 16;
+// c12++;
+// console.log(c12);
+
+
+// let ab = 25;
+// ab++;
+// console.log(ab);
+
+// //Q10 decrement
+//  let R = 16;
+//  R--
+//  console.log(R)
+
+//  //
+ 
+
+
+ // 
+//  let N = 90;
+//  console.log(N--);
+//  console.log(N);
+ 
+
+
+//  let nv = 12;
+// nv--
+// console.log(nv);
+
+ //number
+ let k =14;
+ console.log(typeof k);
+
+ //string
+ let firstName = "Abhinandan";
+ console.log(typeof firstName);
+
+ //bollen
+ let ks = true;
+ console.log(typeof ks);
+
+ //null
+ let rq = null;
+ console.log(typeof rq);
+
+ //undefined
+ let Q;
+ console.log(typeof Q);
+
---
+++ Assignment 2 java/script.js
@@ -0,0 +1,101 @@
+alert("Hey Hello")
+
+function add(){
+    var a= +prompt("Enter a Number");
+    return a;
+}
+var a= add();
+
+// //normal fuction
+// function add( num1, num2){
+//     console.log("this is first value:"+(num1+num2));
+//     console.log(typeof(num1));
+// }
+// add (30,60);
+// //
+// function call(){
+//     console.log ("hello")
+// }
+// call()
+// //
+// function sum( numer1,number2){
+//     console.log(numer1+number2);
+// }
+// sum(35,30);
+// //
+// function add (num1,num2){
+//     console.log(num1+num2)
+
+// }
+// add(242,24)
+// function call(){
+//     let a=+prompt("eneter valid number")
+//     console.log(a);
+// }
+// call();
+// //Arrow function
+// let printSum=(num1,num2,num3)=>{
+//     console.log(`sum of three number :${num1+num2+num3}`);
+// }
+// let abCd=(num1,num2)=>{
+//     console.log(num1-num2);
+// }
+// abCd(23,20);
+// //
+// let print=(num1,num2)=>{
+//     console.log(num1,num2);
+// }
+// print(8,10);
+// //Arow functions without paramerters
+// let printer=()=>{
+//     console.log("this is sum of two number  !")
+// }
+// printer();
+// //
+// let name=()=>{
+//     console.log(`sameer is very clever boy`)
+// }
+// name();
+// //
+// let name2=()=>{
+//     console.log( `I am the greatest person`)
+    
+// }
+// name2();
+// //
+// let multi=()=>{
+//     console.log(28*4);
+// }
+// multi();
+// //
+// let sub=()=>{
+//     console.log(20-12);
+// }
+// sub();
+// //Arrow function without parenthesis
+// let spam=()=> 34/5
+//     console.log( spam());
+//     //
+
+// let div=()=>38+29
+// console.log(div())
+// //Anonymous function
+// const twice= function(){
+//     let a=42;
+//     return a;
+
+// }
+
+// console.log(twice());
+// //
+// const row= function(){
+//     let a ="Abhinandan";
+//     return a;
+// }
+// console.log(row());
+// //IIFE = immediatly invoked function
+// (()=> console.log("functions"))();
+// //
+// (()=> console.log(57))();
+// //
+// (()=> console.log(true))();
